42261c7d47fd72ff9a4eae19122a1a680ad3168c
Adrián Catalán <adrian.catalan@travel-ims.com>
2018-09-22 09:59:37 +0200

    [Chapter11] Changing pages with routing

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ServersService } from './servers.service';
 
 @Component({
   selector: 'app-servers',
@@ -6,28 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-  allowNewServer = false;
-  serverCreationStatus = 'No server was created!';
-  serverName = 'TestServer';
-  serverCreated = false;
-  servers = ['TestServer', 'TestServer 2']
+  private servers: {id: number, name: string, status: string}[] = [];
 
-  constructor() {
-    setTimeout(() => {
-      this.allowNewServer = true;
-    }, 2000)
-  }
+  constructor(private serversService: ServersService) { }
 
   ngOnInit() {
+    this.servers = this.serversService.getServers();
   }
 
-  onCreateServer() {
-    this.serverCreated = true;
-    this.servers.push(this.serverName);
-    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
-  }
-
-  onUpdateServerEvent(event: Event) {
-    this.serverName = ((<HTMLInputElement>event.target).value);
-  }
 }
